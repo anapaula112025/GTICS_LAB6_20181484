39eb302d61b71decae01c59724b662ded18ffde7
Ana <anapaula.hachi@hotmail.com>
2024-05-22 19:17:25 -0500

    first commit

---
+++ src/main/java/org/example/l6_20181484/L620181484Application.java
@@ -0,0 +1,13 @@
+package org.example.l6_20181484;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L620181484Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L620181484Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/l6_20181484/L620181484ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.l6_20181484;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L620181484ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
